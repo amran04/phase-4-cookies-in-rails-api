dbee6ffff3b30782f8ebc179c9775ad0f592c1b8
Amranabdi1 <amranabdikadir101@gmail.c0m>
2022-12-06 11:54:40 +0300

    completed assignment

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
-require_relative "boot"
-
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
+require_relative "boot" 
+ 
+require "rails" 
+# Pick the frameworks you want: 
+require "active_model/railtie" 
 # require "active_job/railtie"
-require "active_record/railtie"
+require "active_record/railtie" 
 # require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
@@ -37,6 +37,21 @@ module Phase4CookiesInRailsApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
+  end
+end
+module MyApp
+  class Application < Rails::Application
+    config.load_defaults 6.1
+    # This is set in apps generated with the --api flag, and removes session/cookie middleware
+    config.api_only = true
+
+    # Must add these lines!
     # Adding back cookies and session middleware
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
